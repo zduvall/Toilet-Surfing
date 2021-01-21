03d12a57103a77d4e115372e0fa516ff1e5d3921
Zachary <duvallzt@gmail.com>
2021-01-21 15:41:13 -0700

    Complete Express Setup up to Routes

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,30 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    app.use(cors());
+}
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+);
