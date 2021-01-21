5d2378d984ecbd48d662592005e83c0a4cca66e9
Zachary <duvallzt@gmail.com>
2021-01-21 15:27:32 -0700

    Complete through beginning express setup

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
