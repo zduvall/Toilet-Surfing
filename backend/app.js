7ef0c0cb8a35b41583c54293c22d94e6265fc54c
Zachary <duvallzt@gmail.com>
2021-01-21 16:34:03 -0700

    Complete error handling, next user authentication

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 
 // Import Routes
 const routes = require('./routes');
@@ -40,6 +42,34 @@ app.use(
 
 // Routes
 app.use(routes);
+
+// Error Handling
+app.use((_req, _res, next) => { // Catch unhandled requests, forward to error handler
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => { // check if err = sequelize error
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => { // format errors
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 // Export
 module.exports = app;
