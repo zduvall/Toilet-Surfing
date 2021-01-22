1abd0f860258e63e2f0c122d355933193c009531
Zachary <duvallzt@gmail.com>
2021-01-21 23:21:55 -0700

    Complete sign up user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,41 +1,9 @@
 const router = require('express').Router();
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
 
 module.exports = router;
-
-
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-
-
-// // remove below -- test setTokenCookie
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     },
-//   })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
-
-// // remove below -- test restoreUser
-// router.get('/restore-user', restoreUser, (req, res) => {
-//   return res.json(req.user);
-// });
-
-// //remove below -- test requireAuth
-// router.get('/require-auth', requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
-
-// // edit below
-// router.post('/test', function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,21 @@
-// const express = require('express')
-// const router = express.Router();
-const router = require('express').Router();
+const express = require('express');
+const asyncHandler = require('express-async-handler');
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
+const router = express.Router();
+
+// Sign up user
+router.post('', asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+  const user = await User.signup({ email, username, password });
+
+  await setTokenCookie(res, user);
+
+  return res.json({
+    user,
+  });
+}));
 
 module.exports = router;
