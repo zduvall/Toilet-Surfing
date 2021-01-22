8ceb733603f21468f2a336bf51f4f56b3c7f01f7
Zachary <duvallzt@gmail.com>
2021-01-21 22:25:18 -0700

    Set up user and session route files

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+module.exports = router;
+
 
 // const asyncHandler = require('express-async-handler');
 // const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
@@ -30,5 +39,3 @@ const router = require('express').Router();
 // router.post('/test', function (req, res) {
 //   res.json({ requestBody: req.body });
 // });
-
-module.exports = router;
