ebdc273b4aae52570fdaa77d76970832ad6e88d8
Zachary <duvallzt@gmail.com>
2021-01-21 23:34:07 -0700

    Complete login validation error handling

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+
 // Log in user
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
