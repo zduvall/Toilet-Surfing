bbc626dc2dc319c40249eefcb860865863262e64
Zachary <duvallzt@gmail.com>
2021-01-21 22:22:24 -0700

    Complete user auth middlewares, next user auth routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
 
-// edit below
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+
+// // remove below -- test setTokenCookie
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     },
+//   })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // remove below -- test restoreUser
+// router.get('/restore-user', restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// //remove below -- test requireAuth
+// router.get('/require-auth', requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // edit below
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
