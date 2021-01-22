8900c1e9c7092089c1ed9a3adbfb7a58648f56bc
Zachary <duvallzt@gmail.com>
2021-01-22 01:09:03 -0700

    Part of restore the XSRF-TOKEN cookie

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,4 +4,32 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
+// Static routes
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 module.exports = router;
