6ae1de288c61ef3951db0659d5b1ded4555eeffe
Zachary <duvallzt@gmail.com>
2021-01-22 00:27:07 -0700

    Complete Redux Provider and BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+// Access store in window during production
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
