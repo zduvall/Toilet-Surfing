abeed60b75d667c50cb3fad287f241587ec4431d
Zachary <duvallzt@gmail.com>
2021-01-25 16:46:52 -0700

    Update database file to include dialect Options

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,11 +13,17 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: "sequelize",
+    seederStorage: 'sequelize',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
