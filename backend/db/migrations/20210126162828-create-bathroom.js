e6d7d371064d5cfc9d9f2f104b8007fd7c6dceed
Zachary <duvallzt@gmail.com>
2021-01-26 09:47:55 -0700

    Update model and migration files to include constraints

--- backend/db/migrations/20210126162828-create-bathroom.js
+++ backend/db/migrations/20210126162828-create-bathroom.js
@@ -6,55 +6,73 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       bathroomOwnerId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' },
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(50),
+        unique: true,
+        allowNull: false,
+        validate: {
+          len: [3, 50],
+        },
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000),
+        validate: {
+          len: [0, 1000],
+        },
       },
       picture: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(2083),
       },
       streetNumber: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(255),
+        allowNull: false,
       },
       route: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(255),
+        allowNull: false,
       },
       locality: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(255),
+        allowNull: false,
       },
       administrativeArea: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(255),
+        allowNull: false,
       },
       postalCode: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(15),
+        allowNull: false,
       },
       country: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(255),
+        allowNull: false,
       },
       lat: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(10, 8),
+        allowNull: false,
       },
       lng: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(10, 8),
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Bathrooms');
-  }
+  },
 };
--- backend/db/models/bathroom.js
+++ backend/db/models/bathroom.js
@@ -1,20 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Bathroom = sequelize.define('Bathroom', {
-    bathroomOwnerId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    picture: DataTypes.STRING,
-    streetNumber: DataTypes.STRING,
-    route: DataTypes.STRING,
-    locality: DataTypes.STRING,
-    administrativeArea: DataTypes.STRING,
-    postalCode: DataTypes.STRING,
-    country: DataTypes.STRING,
-    lat: DataTypes.DECIMAL,
-    lng: DataTypes.DECIMAL
-  }, {});
-  Bathroom.associate = function(models) {
+  const Bathroom = sequelize.define(
+    'Bathroom',
+    {
+      bathroomOwnerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' },
+      },
+      name: { type: DataTypes.STRING(50), unique: true, allowNull: false },
+      description: { type: DataTypes.STRING(1000) },
+      picture: { type: DataTypes.STRING(2083) },
+      streetNumber: { type: DataTypes.STRING(255) },
+      route: { type: DataTypes.STRING(255) },
+      locality: { type: DataTypes.STRING(255) },
+      administrativeArea: { type: DataTypes.STRING(255) },
+      postalCode: { type: DataTypes.STRING(15) },
+      country: { type: DataTypes.STRING(255) },
+      lat: { type: DataTypes.DECIMAL(10, 8) },
+      lng: { type: DataTypes.DECIMAL(10, 8) },
+    },
+    {}
+  );
+  Bathroom.associate = function (models) {
     // associations can be defined here
   };
   return Bathroom;
