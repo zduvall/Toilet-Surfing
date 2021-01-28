e727591851008ac7d19b19fd90c538b1805fe29a
Zachary <duvallzt@gmail.com>
2021-01-28 12:59:37 -0700

    Prep for incorporating google maps api

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,14 +1,18 @@
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 5000,
-    db: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-    },
-    jwtConfig: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    },
+  environment: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 5000,
+  mapsApi: {
+    key: process.env.MAPS_API_KEY,
+    url: `https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}&callback=initMap&libraries=&v=weekly`,
+  },
+  db: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+  },
+  jwtConfig: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
 };
