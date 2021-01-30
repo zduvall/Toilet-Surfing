16593aa3e9e55a4bcf8a4d5b803da77720a9f9fd
Zachary <duvallzt@gmail.com>
2021-01-30 16:07:30 -0700

    Bookings post route validation errors, add booking router to index api

--- backend/routes/api/booking.js
+++ backend/routes/api/booking.js
@@ -18,96 +18,27 @@ router.get(
   })
 );
 
-// Create new bathroom
-const validateCreateBathroom = [
+// Create new booking
+const validateCreateBooking = [
   check('userId')
     .exists({ checkFalsy: true })
-    .withMessage('Bathroom name cannot be empty.')
-    .isLength({ min: 3, max: 50 })
-    .withMessage('Name must be between 4 and 50 characters.')
-    .custom((value) => {
-      return UserBookBathroom.findOne({ where: { name: value } }).then((name) => {
-        if (name) {
-          return Promise.reject('Name already in use by another UserBookBathroom.');
-        }
-      });
-    }),
-  check('description')
+    .withMessage('Booking must be connected to a user.'),
+  check('bathroomId')
     .exists({ checkFalsy: true })
-    .withMessage('Description cannot be empty.')
-    .isLength({ min: 20, max: 200 })
-    .withMessage('Description must be between 20 and 200 characters.'),
-  check('image')
-    .custom((_value, { req }) => {
-      if (!req.file) {
-        throw new Error('Must upload an image.');
-      }
-      return true;
-    })
-    .custom((_value, { req }) => {
-      const regex = new RegExp(/.*\/(apng|avif|jpe?g|png|svg|webp)$/i);
-      if (!regex.test(req.file.mimetype)) {
-        throw new Error(
-          'Upload must be an image (apng, avif, jpeg/jpg, png, svg, webp).'
-        );
-      }
-      return true;
-    }),
-  check('streetNumber')
+    .withMessage('Booking must be connected to a bathroom.'),
+  check('dateTimeStart')
     .exists({ checkFalsy: true })
-    .withMessage('Street number cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Street number required (less than 255 chars).'),
-  check('route')
+    .withMessage('Start time cannot be empty'),
+  check('dateTimeEnd')
     .exists({ checkFalsy: true })
-    .withMessage('Route cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Route required (less than 255 chars).'),
-  check('locality')
-    .exists({ checkFalsy: true })
-    .withMessage('Locality cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Locality required (less than 255 chars).'),
-  check('administrativeArea')
-    .exists({ checkFalsy: true })
-    .withMessage('Administrative area cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Adminiatrative area required (less than 255 chars).'),
-  check('postalCode')
-    .exists({ checkFalsy: true })
-    .withMessage('Postal code cannot be empty.')
-    .isLength({ max: 15 })
-    .withMessage('Postal required (less than 15 chars).'),
-  check('country')
-    .exists({ checkFalsy: true })
-    .withMessage('Country cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Country required (less than 255 chars)'),
-  check('lat')
-    .exists({ checkFalsy: true })
-    .withMessage('Latitude cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Country required (less than 255 chars)'),
-  check('lng')
-    .exists({ checkFalsy: true })
-    .withMessage('Longitude cannot be empty.')
-    .isLength({ max: 255 })
-    .withMessage('Country required (less than 255 chars).'),
-  // check('name').custom((value) => {
-  //   return Bathroom.findOne({ where: { name: value } }).then((name) => {
-  //     if (name) {
-  //       return Promise.reject('Name already in use by another bathroom.');
-  //     }
-  //   });
-  // }),
+    .withMessage('End time cannot be empty'),
   handleValidationErrors,
 ];
 
 router.post(
   '/',
   requireAuth,
-  singleMulterUpload('image'),
-  validateCreateBathroom,
+  validateCreateBooking,
   asyncHandler(async (req, res) => {
     const {
       bathroomOwnerId,
