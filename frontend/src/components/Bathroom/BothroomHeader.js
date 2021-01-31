e5e59f80c4b1a66ff9e9d1db4e1798bb2f1cc9b2
Zachary <duvallzt@gmail.com>
2021-01-30 22:14:17 -0700

    Fix styling on bathroom header info

--- frontend/src/components/Bathroom/BothroomHeader.js
+++ frontend/src/components/Bathroom/BothroomHeader.js
@@ -1,10 +1,31 @@
 import './Bathroom.css';
 
-export default function BathroomHeader({ name, owner }) {
+// import customHook
+import { useWindowWidth } from '../customHooks';
+
+export default function BathroomHeader({ name, owner, snglBRWidth }) {
+  const width = useWindowWidth();
+
   return (
-    <div className='bathroom-header'>
+    <div
+      className='bathroom-header'
+      style={
+        width < 560 || (1110 > width && width > 800)
+          ? { flexDirection: 'column', alignItems: 'flex-start' }
+          : {}
+      }
+    >
       <h1 className='bathroom-header__name'>{name}</h1>
-      <h2 className='bathroom-header__owner'>(by {owner})</h2>
+      <h2
+        style={
+          width < 540 || (1080 > width && width > 800)
+            ? { margin: '0 0 .25rem 0' }
+            : {}
+        }
+        className='bathroom-header__owner'
+      >
+        (by {owner})
+      </h2>
     </div>
   );
 }
