636653542958e69065ecde705ebcf84b4c4c788f
Zachary <duvallzt@gmail.com>
2021-02-01 08:42:57 -0700

    Complete fix styling for single bathroom view

--- frontend/src/components/Bathroom/BothroomHeader.js
+++ frontend/src/components/Bathroom/BothroomHeader.js
@@ -1,31 +1,10 @@
 import './Bathroom.css';
 
-// import customHook
-import { useWindowWidth } from '../customHooks';
-
 export default function BathroomHeader({ name, owner, snglBRWidth }) {
-  const width = useWindowWidth();
-
   return (
-    <div
-      className='bathroom-header'
-      style={
-        width < 560 || (1110 > width && width > 800)
-          ? { flexDirection: 'column', alignItems: 'flex-start' }
-          : {}
-      }
-    >
+    <div className='bathroom-header'>
       <h1 className='bathroom-header__name'>{name}</h1>
-      <h2
-        style={
-          width < 540 || (1080 > width && width > 800)
-            ? { margin: '0 0 .25rem 0' }
-            : {}
-        }
-        className='bathroom-header__owner'
-      >
-        (by {owner})
-      </h2>
+      <h2 className='bathroom-header__owner'>(by {owner})</h2>
     </div>
   );
 }
