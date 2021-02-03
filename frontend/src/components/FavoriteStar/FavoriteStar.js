b16e5a4350d25c05be83615fc57d47ed7f7c3706
Zachary <duvallzt@gmail.com>
2021-02-02 23:19:30 -0700

    Create beginning of star component

---
+++ frontend/src/components/FavoriteStar/FavoriteStar.js
@@ -0,0 +1,10 @@
+export default function FavoriteStar () {
+  return (
+    <>
+      <i className='far fa-star'></i>
+      <i className='fas fa-star'></i>
+    </>
+  );
+}
+
+// top one for not filled in, button filled in
