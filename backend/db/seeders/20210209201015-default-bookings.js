d28c90db54dc893024c3a9281c2e2bd327f7dd14
Zachary <duvallzt@gmail.com>
2021-02-09 13:20:50 -0700

    Add empty seeder files for default bookings and favorites

---
+++ backend/db/seeders/20210209201015-default-bookings.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+      */
+    return queryInterface.bulkInsert(
+      'UserBookBathrooms',
+      [
+        {
+          userId: 1,
+          bathroomId: 1,
+          dateTimeStart: new Date(),
+          dateTimeEnd: new Date(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+      */
+    return queryInterface.bulkDelete('UserBookBathrooms', null, {});
+  },
+};
