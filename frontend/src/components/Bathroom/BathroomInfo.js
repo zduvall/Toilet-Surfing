d4cfdcbb7b874ef93c6632d9891351ff53d4f692
Zachary <duvallzt@gmail.com>
2021-02-14 00:16:05 -0700

    Upadte styling

--- frontend/src/components/Bathroom/BathroomInfo.js
+++ frontend/src/components/Bathroom/BathroomInfo.js
@@ -1,7 +1,7 @@
 export default function BathroomInfo({ curBathroom }) {
   return (
     <>
-      <p>{curBathroom.description}</p>
+      <p className='single-bathroom__desctiption'>{curBathroom.description}</p>
       <p className='single-bathroom__address'>
         {curBathroom.streetNumber} {curBathroom.route}
       </p>
@@ -17,12 +17,11 @@ export default function BathroomInfo({ curBathroom }) {
         {curBathroom.postalCode} {curBathroom.country}
       </p>
       {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>}
-      {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>}
       <p className='lat-lng'>
         {`Lat: ${curBathroom.lat} \u00A0 - \u00A0 Lng: ${curBathroom.lng}`}
       </p>
-      {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>}
-      {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>}
+      {/* {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>}
+      {curBathroom.id === 1 && <p>&nbsp;&nbsp;</p>} */}
     </>
   );
 }
