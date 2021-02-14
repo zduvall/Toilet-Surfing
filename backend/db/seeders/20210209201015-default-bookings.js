d90d8ecb2ae45b2713a6f7fcec58907f6178d8e2
Zachary <duvallzt@gmail.com>
2021-02-13 22:59:44 -0700

    Copy bookings out for one year every two weeks

--- backend/db/seeders/20210209201015-default-bookings.js
+++ backend/db/seeders/20210209201015-default-bookings.js
@@ -5,41 +5,43 @@ const db = require('../models/index');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // create filler bookings
-    const fillerBookings = [];
+    let fillerBookings = [];
     const maxPerUser = 5; // edit this to edit the max # bookings per user
     const numUsers = await db.User.count();
     const numBathrooms = await db.Bathroom.count();
 
     function createDate(userId, bathroomId) {
-      let now = new Date();
-      let daysAhead = Math.ceil(Math.random() * 14);
+      const now = new Date();
+      const daysAhead = Math.ceil(Math.random() * 14);
 
-      let strtHr = Math.ceil(Math.random() * 15) + 6; // random hour between 6AM and 9PM
-      let strtMin = Math.floor(Math.random() * 4) * 15; // random increment between 0 and 3 * 15
+      const strtHr = Math.ceil(Math.random() * 15) + 6; // random hour between 6AM and 9PM
+      const strtMin = Math.floor(Math.random() * 4) * 15; // random increment between 0 and 3 * 15
 
-      let addMin = Math.ceil(Math.random() * 8) * 15; // random 15 minutes to 120 minutes
+      const addMin = Math.ceil(Math.random() * 8) * 15; // random 15 minutes to 120 minutes
+      const newMin = strtMin + addMin;
+
       let endHr, endMin;
-      if (strtMin + addMin <= 45) {
+      if (newMin <= 45) {
         endHr = strtHr;
-        endMin = strtMin + addMin;
-      } else if (strtMin + addMin <= 105) {
+        endMin = newMin;
+      } else if (newMin <= 105) {
         endHr = strtHr + 1;
-        endMin = strtMin + addMin - 60;
+        endMin = newMin - 60;
       } else {
         endHr = strtHr + 2;
-        endMin = strtMin + addMin - 120;
+        endMin = newMin - 120;
       }
 
       // console.log(`${strtHr}:${strtMin} + ${addMin / 15}x15min = ${endHr}:${endMin}`);
 
-      let start = new Date(
+      const start = new Date(
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + daysAhead,
         strtHr,
         strtMin
       );
-      let end = new Date(
+      const end = new Date(
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + daysAhead,
@@ -56,17 +58,59 @@ module.exports = {
       };
     }
 
-    for (let userId = 1; userId <= numUsers; userId++) {
-      let max = Math.ceil(Math.random() * maxPerUser);
+    // make 26 copies of every booking repeated two weeks out every time (to cover one year)
+    function repeatDate(userId, BrId) {
+      let booking = createDate(userId, BrId);
+      const bookingX26 = []
+
+      for(let i = 0; i < 26; i++) {
+        // create new start and end times two weeks out
+        const strtPls2Wks = new Date(booking.dateTimeStart);
+        strtPls2Wks.setDate(strtPls2Wks.getDate() + 14);
+        const endPls2Wks = new Date(booking.dateTimeEnd);
+        endPls2Wks.setDate(endPls2Wks.getDate() + 14);
+        
+        // copy and update attributes on newBooking
+        const newBooking = { ...booking };
+        newBooking.dateTimeStart = strtPls2Wks;
+        newBooking.dateTimeEnd = endPls2Wks;
+
+        bookingX26.push(newBooking)
+        booking = newBooking
+      }
+
+      return bookingX26;
+    }
+
+    for (let userId = 3; userId <= numUsers; userId++) {
+      const max = Math.ceil(Math.random() * maxPerUser);
       for (let i = 0; i < max; i++) {
-        let randBrId = Math.ceil(Math.random() * numBathrooms);
-        fillerBookings.push(createDate(userId, randBrId));
+        const randBrId = Math.ceil(Math.random() * numBathrooms);
+        fillerBookings = [...fillerBookings, ...repeatDate(userId, randBrId)];
       }
     }
 
     return queryInterface.bulkInsert(
       'UserBookBathrooms',
-      [...fillerBookings],
+      [
+        // {
+        //   userId: 2,
+        //   bathroomId: 3,
+        //   dateTimeStart: new Date(2021, 1, 15, 10),
+        //   dateTimeEnd: new Date(2021, 1, 15, 12),
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // },
+        // {
+        //   userId: 3,
+        //   bathroomId: 2,
+        //   dateTimeStart: new Date(2021, 1, 15, 11),
+        //   dateTimeEnd: new Date(2021, 1, 15, 13),
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // },
+        ...fillerBookings,
+      ],
       {}
     );
   },
